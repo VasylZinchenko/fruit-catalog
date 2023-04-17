b69b2f19d42118bf293da8ab4e9038034760ff22
Vasyl Zinchenko <vasyl.zinchenko1991@gmail.com>
2023-04-17 03:41:24 +0300

    adding unplugin-i18n

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { createI18n } from "vue-i18n";
-import { Router } from "./router";
-
 import App from "./App.vue";
 import mdiVue from "mdi-vue/v3";
-import messages from "@intlify/vite-plugin-vue-i18n/messages";
 import * as mdijs from "@mdi/js";
+import messages from "@intlify/unplugin-vue-i18n/messages";
+import { Router } from "./router";
 import "@unocss/reset/tailwind.css";
 import "uno.css";
 import "./styles/base.css";
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
 import { defineConfig } from "vite";
+import Unocss from "unocss/vite";
 import { resolve } from "pathe";
-import Vue from "@vitejs/plugin-vue";
-import svgLoader from "vite-svg-loader";
+import vue from "@vitejs/plugin-vue";
+import VueI18n from "@intlify/vite-plugin-vue-i18n";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import path from "path";
 import Components from "unplugin-vue-components/vite";
 import ViteFonts from "vite-plugin-fonts";
-import Unocss from "unocss/vite";
-import VueI18n from "@intlify/vite-plugin-vue-i18n";
+import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       "/@": resolve(__dirname, "./src"),
+      "vue-i18n": "vue-i18n/dist/vue-i18n.runtime.esm-bundler.js",
     },
   },
   plugins: [
-    Vue(),
+    vue(),
     // https://github.com/jpkleemans/vite-svg-loader
     svgLoader(),
     // https://github.com/antfu/vite-plugin-components
@@ -37,6 +40,9 @@ export default defineConfig({
     // https://github.com/intlify/vite-plugin-vue-i18n
     VueI18n({
       include: [resolve(__dirname, "./locales/**")],
+    }),
+    VueI18nPlugin({
+      include: path.resolve(__dirname, "./locales/**"),
     }),
   ],
   optimizeDeps: {
